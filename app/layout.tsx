8466916ff06225e91703dfb2adac59c72a190d77
James.Harrison <James.Harrison@Serko.com>
2022-11-08 14:43:59 +1300

    Simple NavBar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,28 @@
+import Head from 'next/head';
 import React, { FC } from 'react';
+import { NavBar } from '../lib/ui';
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
+const siteTitle = 'Budget Tracker';
+
 const RootLayout: FC<RootLayoutProps> = ({ children }) => {
   return (
     <html>
-      <head></head>
-      <body>{children}</body>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          name="description"
+          content="Welcome to my budget tracking tool!"
+        />
+        <meta name="og:title" content={siteTitle} />
+      </Head>
+      <body>
+        <NavBar />
+        {children}
+      </body>
     </html>
   );
 };
---
+++ lib/ui/navbar/NavBar.tsx
@@ -0,0 +1,5 @@
+import { FC } from 'react';
+
+export const NavBar: FC = () => {
+  return <div> I am a NavBar</div>;
+};
