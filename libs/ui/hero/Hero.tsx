d3506b692cbf0e931308e14e971cc3697411a560
James.Harrison <James.Harrison@Serko.com>
2022-11-15 17:17:41 +1300

    Styling

--- libs/ui/hero/Hero.tsx
+++ libs/ui/hero/Hero.tsx
@@ -1,11 +1,9 @@
 export const Hero: React.FC = () => {
   return (
-    <div className="hero min-h-screen bg-base-200 ">
+    <div className="hero min-h-screen">
       <div className="hero-content text-center">
-        <div className="max-w-xl backdrop-blur-sm bg-base-100/50 p-4 rounded-lg shadow-2xl">
-          <h1 className="font-hero text-9xl font-bold font-effect-3d">
-            Budget Tracker
-          </h1>
+        <div className="max-w-xl p-4">
+          <h1 className="font-hero text-9xl font-effect-3d">Budget Tracker</h1>
           <p className="py-6">For new zealand banks</p>
         </div>
       </div>
