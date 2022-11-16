4a1691e8bf0cd8fb06865ac7d2a436f32ce0aa48
James.Harrison <James.Harrison@Serko.com>
2022-11-16 20:13:35 +1300

    Update paragraph

--- libs/ui/hero/Hero.tsx
+++ libs/ui/hero/Hero.tsx
@@ -4,7 +4,7 @@ export const Hero: React.FC = () => {
       <div className="hero-content text-center">
         <div className="max-w-xl p-4">
           <h1 className="font-hero text-9xl font-effect-3d">Budget Tracker</h1>
-          <p className="py-6">For new zealand banks</p>
+          <p className="py-6">for New Zealand banks</p>
         </div>
       </div>
     </div>
