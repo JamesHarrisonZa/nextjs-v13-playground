33600172c5d49d9ed94fcf33413ea139b65e426d
James.Harrison <James.Harrison@Serko.com>
2023-02-14 15:14:47 +1300

    Better loading component with spinner

--- app/payments/loading.tsx
+++ app/payments/loading.tsx
@@ -1,5 +1,12 @@
 const Loading = () => {
-  return <p>Loading...</p>;
+  return (
+    <div className="grid min-h-screen place-content-center">
+      <div className="flex items-center gap-2 text-gray-500">
+        <span className="h-6 w-6 block rounded-full border-4 border-t-blue-300 animate-spin"></span>
+        loading...
+      </div>
+    </div>
+  );
 };
 
 export default Loading;
