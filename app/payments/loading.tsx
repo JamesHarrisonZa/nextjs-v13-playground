9ea392fab15b086e27e18f24d095b8008afbc128
James.Harrison <James.Harrison@Serko.com>
2023-02-14 15:55:12 +1300

    Change min height to 100% rather than 100vh

--- app/payments/loading.tsx
+++ app/payments/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="grid min-h-screen place-content-center">
+    <div className="grid min-h-full place-content-center">
       <div className="flex items-center gap-2 text-gray-500">
         <span className="h-6 w-6 block rounded-full border-4 border-t-blue-300 animate-spin"></span>
         loading...
